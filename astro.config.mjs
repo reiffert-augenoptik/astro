9ecb81f9a92b17d2140348eb9d9de547bfb6193f
spuxx1701 <accounts@spuxx.dev>
2025-10-03 21:54:08 +0200

    chore: Add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
+import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   devToolbar: { enabled: false },
 });
