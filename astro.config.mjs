04161a79b5c4cf60eb2955741919fc0c64542a01
spuxx1701 <accounts@spuxx.dev>
2025-10-04 15:09:23 +0200

    fix: Fix site and base paths for github page to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://reiffert-augenoptik.github.io/astro/',
+  base: '/astro/',
   integrations: [mdx()],
   devToolbar: { enabled: false },
 });
