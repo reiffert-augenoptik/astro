66183110f8f993322e85e7f0bf2829170230705b
spuxx1701 <accounts@spuxx.dev>
2025-10-04 14:39:00 +0200

    feat: Implement footer

--- README.md
+++ README.md
@@ -14,6 +14,10 @@ cd astro
 npm run dev
 ```
 
+### Changing business details
+
+Some of the business details (e.g. name, adress or opening hours) are maintained centrally in the [`app.config.ts`](/app.config.ts) file. When edited there, they will update across the website.
+
 ## TODO
 
 - Basically everything
---
+++ app.config.ts
@@ -0,0 +1,16 @@
+export const appConfig = {
+  title: 'REIFFERT Augenoptik & Hörakustik',
+  businessInfo: {
+    adress: 'Ludwig-Pfeuffer-Ring 6, Giebelstadt, BY, 97232, Germany',
+    phone: '09334 9753043',
+    mail: 'post@reiffert-giebelstadt.de',
+    openingHours: [
+      'Mo. 09:00 bis 18:00 Uhr',
+      'Di. 09:00 bis 18:00 Uhr',
+      'Mi. 09:00 bis 18:00 Uhr',
+      'Do. 09:00 bis 18:00 Uhr',
+      'Fr. 09:00 bis 18:00 Uhr',
+      'Sa. 09:00 bis 14:00 Uhr',
+    ],
+  },
+};
