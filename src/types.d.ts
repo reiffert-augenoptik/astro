9b24903de38f37d0854c63bfae7dd7f69a118666
spuxx1701 <accounts@spuxx.dev>
2025-10-05 12:53:33 +0200

    fix: Fix pages without frontmatter breaking

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,13 +4,13 @@ declare global {
      * The text alignment of the page. By default, text is left-aligned.
      * @default 'start'
      */
-    align: 'start' | 'center' | 'end';
+    align?: 'start' | 'center' | 'end';
     /**
      * The maximum width of the content. Defaults to `medium`.
      * For values, see {@link [variables](./styles/variables.css)}.
      * @default 'medium'
      */
-    width: 'small' | 'medium' | 'large' | 'full';
+    width?: 'small' | 'medium' | 'large' | 'full';
   }
 }
 
