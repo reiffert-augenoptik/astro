eb77f4e3a2f7326896bb580abeae6f112ebe0005
spuxx1701 <accounts@spuxx.dev>
2025-11-09 11:50:00 +0100

    feat: Implement working sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import mdx from '@astrojs/mdx';
+import astroIcon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://reiffert-augenoptik.github.io/astro/',
   base: '/astro/',
-  integrations: [mdx()],
+  integrations: [mdx(), astroIcon()],
   devToolbar: { enabled: false },
+  build: {
+    inlineStylesheets: 'always',
+  },
   vite: {
     plugins: [tsconfigPaths()],
   },
