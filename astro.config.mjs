17c26c4e9814758309147fc1bab42c3f36a28e5d
@LeopoldHock <leopold.hock@deutschebahn.com>
2026-01-02 09:53:09 +0100

    chore: Add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import mdx from '@astrojs/mdx';
 import astroIcon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://reiffert-augenoptik.github.io/astro/',
   base: '/astro/',
-  integrations: [mdx(), astroIcon()],
+  integrations: [mdx(), astroIcon(), sitemap()],
   devToolbar: { enabled: false },
   build: {
     inlineStylesheets: 'always',
