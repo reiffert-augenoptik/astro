6b4a0abd551448cc8da38e21ddfcd56f48de3f6e
Kraut-Bob <reiffert.david@gmail.com>
2026-06-04 10:00:52 +0200

    Kalender Integration

--- app.config.ts
+++ app.config.ts
@@ -6,6 +6,8 @@ export const appConfig = {
     adress: 'Ludwig-Pfeuffer-Ring 6, Giebelstadt, BY, 97232, Germany',
     phone: '09334 9753043',
     mail: 'post@reiffert-giebelstadt.de',
+    terminUrl:
+      'https://optikertermin2.de/v2/index.php?encId=0B88A447-43F3-4465-8672-6DFE52734CDB',
     openingHours: {
       monday: 'Mo. 09:00 bis 18:00 Uhr',
       tuesday: 'Di. 09:00 bis 18:00 Uhr',
